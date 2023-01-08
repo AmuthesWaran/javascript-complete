7a02ef3a6599a7470056ff5640493cad040701e8
Amutheswaran <amutheswaran@outlook.com>
2023-01-08 21:33:59 +0530

    day 9 chapter 6 part 3

--- 04-arrays/_01-notes.js
+++ 04-arrays/_01-notes.js
@@ -38,7 +38,7 @@ list.splice(1, 2)
 
 console.log(list)
 
-list.splice(1,0,'New Second element')
+list.splice(1, 0, 'New Second element')
 
 console.log(list)
 
@@ -53,7 +53,94 @@ list.forEach(list => {
 })
 
 
-list.forEach(function(list, index){
+list.forEach(function (list, index) {
     console.log(list, index)
 })
 
+// Dynamic for loop using function
+// function counting(count) {
+
+    //for loop
+    for (let i = 1; i <= 10; i++) {
+        console.log(`Counting... ${i}`);;
+    }
+// }
+
+
+// counting(10);
+
+console.log(list)
+for (let i = 0; i < list.length; i++){
+    console.log(list[i]);
+}
+
+
+console.log(list)
+for (let i = list.length - 1; i >= 0; i--){
+    console.log(list[i]);
+}
+
+const notes0 = ['Note1', 'Note2', 'Note3', 'Note4', 'Note5']
+
+console.log(notes0.indexOf('Note2'));
+console.log(notes0.indexOf('note2'));
+
+
+const notes1 = [
+    // {}, 
+    {
+    title: 'My trip',
+    body: 'i will be going to office next week'
+}, {
+    title: 'New hobby',
+    body: 'origami'
+}, {
+    title: 'To buy',
+    body: 'A notebook'
+}]
+
+
+// console.log(notes1.indexOf({}))
+
+// let obj1 = {}
+// let obj2 = {}
+
+// console.log(obj1 === obj2);
+
+// let obj3 = {}
+// let obj4 = obj3
+
+// console.log(obj3 === obj4);
+
+
+// looks for the first index
+// const index = notes1.findIndex(function (note, index) {
+//     console.log(note)
+//     return note.title === 'My trip'    
+// })
+
+// console.log(index);
+
+
+// const findNote = function (notes, noteTitle){
+//     const index = notes.findIndex(function (note, index){
+//         console.log(note.title);
+//         return note.title.toLowerCase() === noteTitle.toLowerCase()
+//     })
+
+//     if (index >= 0){
+//         console.log(notes[index]);
+//     }
+//     else{
+//         console.log('404 Not found');
+//     }
+// }
+
+const findNote = function (notes, noteTitle){
+    return notes.find(function (note, index){
+        return note.title === noteTitle
+    })
+}
+
+const note = findNote(notes1, 'To buy')
+console.log(note)
--- 04-arrays/_02-todo.js
+++ 04-arrays/_02-todo.js
@@ -26,3 +26,39 @@ const todo0 = ['Wake up', 'Eat', 'Learn', 'Work', 'Sleep']
 todo0.forEach((todo, index) => {
     console.log((index + 1) + '.', todo);
 });
+
+// console.log();
+// for (let i = 0; i < todo0.length; i++) {
+//     console.log((i+1) + '.', todo0[i]);
+
+// }
+
+const todos1 = [{
+    todo: 'Wake Up',
+    complete: true
+}, {
+    todo: 'Eat',
+    complete: true
+}, {
+    todo: 'Learn',
+    complete: true
+}, {
+    todo: 'Work',
+    complete: true
+}, {
+    todo: 'Sleep',
+    complete: true
+}]
+
+
+const deleteTodo = function(todos, todoText){
+    const index = todos.findIndex(function (todo) {
+        return todo.todo.toLowerCase() === todoText.toLowerCase()
+    })
+    if(index > -1){
+        todos.splice(index, 1)
+    }
+}
+
+deleteTodo(todos1, 'Sleep')
+console.log(todos1)
