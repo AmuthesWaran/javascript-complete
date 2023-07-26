d2af298defac37ee7d12f0a40537a46b528e27f7
Amutheswaran <amutheswaran@outlook.com>
2023-07-26 11:45:21 +0530

    poc extended done - csv file generated

--- 00-js-simple-programs/emalid-extract-to-csv.js
+++ 00-js-simple-programs/emalid-extract-to-csv.js
@@ -1,15 +1,20 @@
-const { readFile } = require('fs/promises')
+const { readFile, writeFile } = require('fs/promises')
 
 
 var text;
 
 var splittedArray;
 
-var splittedArrayclean = [];
+var splittedArrayclean = []
+var fnameclean = []
+
+var outputcsv;
+
 
 async function content(path) {
     return await readFile(path, 'utf8')
 }
+
 
 async function getAsText() {
     text = await content('./emailid.txt')
@@ -18,15 +23,35 @@ async function getAsText() {
     console.log("\n");
     splittedArray = text.split(";")
 
-    console.log(splittedArray);
+    // console.log(splittedArray);
 
     for (let i = 0; i < splittedArray.length; i++) {
         splittedArrayclean.push(splittedArray[i].trim());
+        fnameclean.push(splittedArray[i].trim().split('.')[0])
+    }
+
+    // console.log(splittedArrayclean)
+    // console.log(fnameclean)
+    const csvRows = [];
+
+    const headers = ["firstname", "email"];
+
+    csvRows.push(headers.join(","))
+
+
+
+    for (let i = 0; i < fnameclean.length; i++) {
+        csvRows.push(fnameclean[i] + "," + splittedArrayclean[i]);
 
     }
 
-    console.log(splittedArrayclean);
+    outputcsv = csvRows.join('\n')
+
+    writeFile("./output.csv", outputcsv)
+
 }
+
+
 
 getAsText()
 
